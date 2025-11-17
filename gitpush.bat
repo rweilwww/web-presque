797ad32ede3e6e8b56b0108f1ea5e04fff9908c0
rweilwww <github@shaw-weil.com>
2025-11-17 11:12:36 -0500

    from inDesign 2025-11-17 16:10

--- gitpush.bat
+++ gitpush.bat
@@ -1,14 +1,9 @@
 
 cd /inetpub/wwwroot/w/presque
-git init
-            git config --global --add safe.directory C:/inetpub/wwwroot/w/presque
-   git remote add origin https://github.com/rweilwww/web-presque.git
-   git remote -v
 
-        
 
 git add .
-git commit -m "from inDesign 2025-11-10 18:48" --no-edit
+git commit -m "from inDesign 2025-11-17 16:10" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
